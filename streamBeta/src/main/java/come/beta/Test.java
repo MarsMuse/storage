62288d57e5fa44d107c51dd25cce42239629e665
betaIsMars <13889146832@163.com>
2017-04-19 15:38:39 +0800

    新建测试类

---
+++ streamBeta/src/main/java/come/beta/Test.java
@@ -0,0 +1,15 @@
+package come.beta;
+
+/**
+ * 
+ * @ClassName:  Test   
+ * @Description:TODO(测试数据)   
+ * @author: 成都正合联众网络科技有限公司
+ * @date:   2017年4月19日 下午3:37:31   
+ *     
+ * @Copyright: 2017 
+ *
+ */
+public class Test {
+
+}
